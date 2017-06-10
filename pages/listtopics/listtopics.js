85b9ac0dd6585b72353ec34811dd38df0070a9d8
lilin <lilin@myrual.me>
2017-06-11 00:40:48 +0800

    try to show list

---
+++ pages/listtopics/listtopics.js
@@ -0,0 +1,91 @@
+// videointopics.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    opentopics: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    console.log(that.data.token);
+    app.request()
+      .get('https://dd.doudouapp.com/api/v1/openbattles.json')
+      .query({
+        appid: app.globalData.appid,
+        appsecret: app.globalData.appsecret,
+        user_id: app.globalData.userid,
+        session: app.globalData.usersession
+      })
+      .end()
+      .then(function (res) {
+        console.log(res.data[0])
+        console.log(res.data[1])
+        console.log(res.data[2])
+        console.log(res.data[3])
+        that.setData({
+          opentopics: res.data
+        })
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  choosetopic: function (test) {
+    wx.navigateTo({
+      url: '../videointopics/videointopics?topicid=' + test.target.id
+    })
+  }
+})
